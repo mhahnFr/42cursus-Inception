144d49810f03505c1e666a3a4b8768e498e4632a
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-10-26 13:39:37 +0200

    Made wp init script readable

--- srcs/requirements/wordpress_phpfpm/arsch.sh
+++ srcs/requirements/wordpress_phpfpm/arsch.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
-cd /var/www/html \
- && wp core download --allow-root \
- && wp config create --dbname=${MARIADB_NAME} \
-                     --dbuser=${MARIADB_USER} \
-                     --dbpass=${MARIADB_PWD} \
-                     --dbhost=${MARIADB_HOST} \
-                     --locale=en_GER \
-                     --allow-root \
- && wp core install --url=${DOMAIN_NAME} \
-                    --title=${WP_BLOG_TITLE} \
-                    --admin_user=${WP_ROOT_USER} \
-                    --admin_password=${WP_ROOT_PWD} \
-                    --admin_email=${WP_ROOT_MAIL} \
-                    --allow-root \
- && wp user create ${WP_USER} ${WP_USER_MAIL} --role=administrator --user_pass=${WP_USER_PWD} --allow-root
+cd /var/www/html
+
+wp core download --allow-root
+
+wp config create --dbname=${MARIADB_NAME} \
+                 --dbuser=${MARIADB_USER} \
+                 --dbpass=${MARIADB_PWD} \
+                 --dbhost=${MARIADB_HOST} \
+                 --locale=en_GER \
+                 --allow-root
+
+wp core install --url=${DOMAIN_NAME} \
+                --title=${WP_BLOG_TITLE} \
+                --admin_user=${WP_ROOT_USER} \
+                --admin_password=${WP_ROOT_PWD} \
+                --admin_email=${WP_ROOT_MAIL} \
+                --allow-root
+
+wp user create ${WP_USER} ${WP_USER_MAIL} --role=administrator \
+                                          --user_pass=${WP_USER_PWD} \
+                                          --allow-root
 
 php-fpm8 -F
